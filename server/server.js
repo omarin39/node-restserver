3ed03095ba0142836b39b5224f3eb854eba1ecd4
omarin39 <omaralvarez01.oaa@gmail.com>
2021-01-27 19:08:13 -0600

    se corrige start para servicios

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/usuario', function(req, res) {
-    res.json('getUsuario')
+    res.json('getUsuario LOCAL!!')
 })
 
 app.post('/usuario', function(req, res) {
