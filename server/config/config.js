634bada9204a21ea4df7aa65230c768555518460
omarin39 <omaralvarez01.oaa@gmail.com>
2021-01-28 22:27:08 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,22 @@
-/*
- * Creacion de PUERTO 
- */
+/*==========================
+ * Creación de puerto
+ ===========================*/
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+/*==========================
+ * Entorno
+ ===========================*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/*==========================
+ * Base de datos
+ ===========================*/
+
+let urlDB;
+
+urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://omarinMongo:9jFmDlsrG6sQxWdJ@cluster0.drod9.mongodb.net/cafe';
+
+process.env.URLDB = urlDB;
