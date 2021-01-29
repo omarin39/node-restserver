9a93599af33aae6278d3a374ea7566652ab18664
omarin39 <omaralvarez01.oaa@gmail.com>
2021-01-28 23:06:52 -0600

    tercer

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.PORT = process.env.PORT || 3000;
 
 
 /*==========================
- * Entorno as
+ * Entorno asasdasdsad
  ===========================*/
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
