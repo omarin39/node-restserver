60cc0e61d92901acd237f406da0066c12787a757
omarin39 <omaralvarez01.oaa@gmail.com>
2021-02-02 15:07:31 -0600

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -39,3 +39,10 @@ let urlDB;
 urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://omarinMongo:9jFmDlsrG6sQxWdJ@cluster0.drod9.mongodb.net/cafe';
 
 process.env.URLDB = urlDB;
+
+
+/*==========================
+ * Google Client ID
+ ===========================*/
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || "301262113138-erdnqvhuh3jnmulbsaf0ht20f6q86ies.apps.googleusercontent.com";
